886b6f96d965e45e8df5428abc684c8af94edc73
Dan <danlowry1991@googlemail.com>
2025-08-12 19:02:33 +0100

    Remove redundant eslint config in root

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  root: true,
-  // This tells ESLint to load the config from the package `@cursor-usage/config`
-  extends: ["@cursor-usage/config/library"],
-  settings: {},
-};
