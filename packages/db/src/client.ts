e229bd108154c9c43c659e20f71ba5a4ab93176d
Dan <danlowry1991@googlemail.com>
2025-08-19 21:54:52 +0100

    Remove disable linter in db client

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,16 +1,14 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const PrismaClientModule: any = require('@prisma/client');
-const PrismaClientCtor: any = PrismaClientModule?.PrismaClient;
+import { PrismaClient } from "@prisma/client";
 
 // Persist a single PrismaClient instance across hot reloads in dev.
 const globalForPrisma: any = globalThis as any;
 const GLOBAL_KEY = '__cursor_usage_db_prisma__';
 
-export const prisma: any = globalForPrisma[GLOBAL_KEY] ?? new PrismaClientCtor({
+export const prisma: any = globalForPrisma[GLOBAL_KEY] ?? new PrismaClient({
   log: process.env.NODE_ENV === 'production' ? ['error'] : ['query', 'error', 'warn'],
-});
+  });
 
 if (!globalForPrisma[GLOBAL_KEY]) globalForPrisma[GLOBAL_KEY] = prisma;
 
-export default prisma; 
+export default prisma;
