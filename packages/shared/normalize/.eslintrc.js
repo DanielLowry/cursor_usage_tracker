14c6fd7dba1f94bdaf04725e00a3a519a8789fc0
Dan <danlowry1991@googlemail.com>
2025-08-24 09:44:47 +0100

    Setup the right files to ignore by linter

---
+++ packages/shared/normalize/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [require.resolve("@cursor-usage/config/library")],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  ignorePatterns: ["**/*.js"],
+};
